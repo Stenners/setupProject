bde535aa5768390438d677d76cee8a57d128b151
Anthony <anthony.stenmark@accenture.com>
2017-11-09 12:53:30 +1100

    Clean up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ContactForm from './components/ContactForm';
-import './sass/styles.scss';
-
-ReactDOM.render(<ContactForm />, document.getElementById('root'));
+/* Do stuff*/
